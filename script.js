af43c4d66a0785c825c817c13c5301a812672fe1
Jessie <FranceJessieJ@gmail.com>
2020-05-18 15:13:53 -0600

    Added some x and y axes.

--- script.js
+++ script.js
@@ -6,15 +6,41 @@ var url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/m
 var width = 800;
 var height = 400;
 
-var svg = d3.select("body")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+// Declare svg d3 object.
+var svg = d3.select(".d3-div")
+            .append("svg")
+            .attr("width", width*1.15)
+    .attr("height", height*1.15);
+
+
+
+// Month to quarter lookup object for converting months
+// into respective economic quarter.
+var month2QuarterMap = new Map();
+month2QuarterMap.set("01", "Q1");
+month2QuarterMap.set("04", "Q2");
+month2QuarterMap.set("07", "Q3");
+month2QuarterMap.set("10", "Q4");
+
 
 // Get json data using D3 fetch method.
 d3.json(url)
   // Promise as function of data...
-  .then(function(data) {
+    .then(function(data) {
+
+	// Add future y-label
+	svg.append('text')
+	.attr('transform', 'rotate(-90)')
+	.attr('x', -300)
+	.attr('y', 80)
+	.text('Gross Domestic Product');
+	
+	// Add future x-label.
+	svg.append('text')
+	.attr('x', width / 2 + 90)
+	.attr('y', height + 50)
+        .text('Time (years)')
+        .attr('class', 'info');
 
        // Body section tag.
        var section = d3.select("body")
@@ -32,18 +58,59 @@ d3.json(url)
       let dates = data["data"].map(nestedArr => nestedArr[0]);
       let gdpData = data["data"].map(nestedArr => nestedArr[1]);
 
-      // Create d3 linear scale object from gdp data.
-      let linearScale = d3.scaleLinear()
-                        .domain( [ 0, d3.max(gdpData) ] )
-                        .range( [ 0, height ] );
+      // Extract the quarters and years from the data.
+      let yearAndQuarter = data["data"].map( (dateStr)=>{
+	  let dateArr = dateStr[0].split('-');
+	  return dateArr[0] + " " + month2QuarterMap.get(dateArr[1]);
+      });
+      
+      // Get Unix epoch style dates too.
+      let unixDates = data["data"].map( item => new Date(item[0]) );
 
-      // Create array of scaled GDP using the d3 linear scale.
-      let gdpScaled = gdpData.map( gdpVal => linearScale(gdpVal) );
-      console.log(d3.max(gdpScaled)); // height
+      // Build time scale by first finding maxima point.
+      // Get last date in set. (june 2015)
+      let xMax = new Date(d3.max(unixDates)); 
+      // add 3 months to that last date. (sept 2015). this maybe creates a cushion?
+      xMax.setMonth(xMax.getMonth() + 3);
+      let timeScale = d3.scaleTime()
+	             .domain([d3.min(unixDates), xMax])
+                     .range([0, width]);
+
+      // Use time scale to create x-axis, with x-axis placed on bottom.
+      let xAxis = d3.axisBottom()
+                    .scale(timeScale);
+      
+
+
+      // Plot x-axis.
+      let xAxisPlot = svg.append("g")
+          .call(xAxis)
+          .attr("id", "x-axis")
+          .attr("transform", "translate(60, 400)");
 
 
       
+      // Create d3 linear scale object from gdp data.
+      let linearScale = d3.scaleLinear()
+                        .domain( [ 0, d3.max(gdpData) ] )
+          .range( [ 0, height ] );
 
+
+      // Create array of scaled GDP using the d3 linear scale.
+      let gdpScaled = gdpData.map( gdpVal => linearScale(gdpVal) );
+
+      // set domain and range for y-axis.
+      let yAxisScale = d3.scaleLinear()
+          .domain([0, d3.max(gdpData)])
+          .range([height, 0]);
+
+      let yAxis = d3.axisLeft(yAxisScale);
+
+      let yAxisPlot = svg.append("g")
+          .call(yAxis)
+          .attr("id", "y-axis")
+          .attr("transform", "translate(60, 0)");
+      
 
 
       
@@ -51,6 +118,6 @@ d3.json(url)
   })
   // Log error if promise on loading data not fulfilled.
   .catch(function(error) {
-      console.log("Data did not load.");
+      console.log(error);
   });
 
